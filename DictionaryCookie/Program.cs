6f9d3022d0cfbd7a6d7ece505c73f1edd41599c5
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-23 14:50:49 -0300

    Finish

--- DictionaryCookie/Program.cs
+++ DictionaryCookie/Program.cs
@@ -11,6 +11,26 @@ namespace DictionaryCookie
         static void Main(string[] args)
         {
             Dictionary<string,string> cookies = new Dictionary<string,string>();
+            cookies["user"] = "Maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "996636643";
+            cookies["phone"] = "877854125";
+            cookies.Remove("email");
+            Console.WriteLine(cookies["phone"]);
+            if (cookies.ContainsKey("email"))
+                Console.WriteLine(cookies["email"]);
+            else
+                Console.WriteLine("There is no 'email' key");
+
+            Console.WriteLine("Cookies size : " + cookies.Count);
+            Console.WriteLine("ALL COOKIES : ");
+            foreach (var item in cookies)
+            {
+                Console.WriteLine(item.Value);
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
