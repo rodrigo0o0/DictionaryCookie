56605c88b2b25a3ab6a30fda73f31c189f700dde
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-23 14:43:11 -0300

    First Commit

---
+++ DictionaryCookie/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryCookie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,string> cookies = new Dictionary<string,string>();
+        }
+    }
+}
